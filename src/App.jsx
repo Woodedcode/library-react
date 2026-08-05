734b4fe8ab67873d9717d2da628388c72508d899
Ryan Woods <Woodedcode@gmail.com>
2026-08-04 22:15:06 -0500

    Added footer, footer links, and added on a Books section

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import Nav from './components/Nav.jsx'
-import Landing from './components/Landing.jsx'
-import Highlights from './components/Highlights.jsx'
-import Featured from './components/Featured.jsx';
-import Discounted from './components/Discounted.jsx';
+import Nav from "./components/Nav.jsx";
+import Footer from "./components/Footer.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Books from "./pages/Books.jsx";
 
 function App() {
-
-return (
-    <div className="App">
+  return (
+    <Router>
+      <div className="App">
         <Nav />
-        <Landing />
-        <Highlights />
-        <Featured />
-        <Discounted />
-    </div>
-);
-
+        <Route path="/" exact component={Home}/>
+        <Route path="/books" component={Books}/> 
+        <Home />
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
