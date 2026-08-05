d5097768f318903218589a3dc608cc85d4037bab
Ryan Woods <Woodedcode@gmail.com>
2026-08-05 11:18:23 -0500

    upgraded all a's to Links, working on Book link

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import Footer from "./components/Footer.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Books from "./pages/Books.jsx";
-import {books} from './data.js';
-import React, {useState} from "react";
+import { books } from "./data.js";
+import BookInfo from "./pages/BookInfo.jsx";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Nav />
-        <Route path="/" exact component={Home}/>
-        <Route path="/books" render={() => <Books books={books}/>}/>
-        <Home />
+          <Route path="/" exact component={Home} />
+          <Route path="/books" exact render={() => <Books books={books} />} />
+          <Route path="/books/1" render={() => <BookInfo books={books} />} />
         <Footer />
       </div>
     </Router>
