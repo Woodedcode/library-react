d29a377cd8f151d000f105fc9364d6eb20722553
Ryan Woods <Woodedcode@gmail.com>
2026-08-05 10:25:48 -0500

    Got to woring/sorting spot

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import {books} from '../data.js';
+import React, { useState } from 'react';
+import { books as initialBooks } from '../data.js';
 import Book from '../components/ui/Book.jsx';
 
 const Books = () => {
-  const Books = ({books: initialBooks}) => {
-        const [books, setBooks] = useState(initialBooks)
+  const [books, setBooks] = useState(initialBooks);
+
+  function filterBooks(filter) {
+    console.log(filter);
+
+    if (filter === 'LOW_TO_HIGH') {
+      setBooks(
+        books.slice().sort((a, b) => {
+          return (
+            (a.salePrice || a.originalPrice) -
+            (b.salePrice || b.originalPrice)
+          );
+        })
+      );
     }
 
-    function filterBooks(filter) {
-      console.log(filter)
-      if (filter === 'LOW_TO_HIGH') {
-        setBooks(books.slice().sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)))
-        if (filter === 'HIGH_TO_LOW') {
-          setBooks(books.slice().sort((a, b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice)))
-        }
-      }
-      if (filter === 'RATING') {
-        setBooks(books.slice().sort((a,b) => b.rating - a.rating))
-      }
+    if (filter === 'HIGH_TO_LOW') {
+      setBooks(
+        books.slice().sort((a, b) => {
+          return (
+            (b.salePrice || b.originalPrice) -
+            (a.salePrice || a.originalPrice)
+          );
+        })
+      );
     }
+
+    if (filter === 'RATING') {
+      setBooks(
+        books.slice().sort((a, b) => {
+          return b.rating - a.rating;
+        })
+      );
+    }
+  }
+
   return (
     <div id="books__body">
       <main id="books__main">
@@ -26,16 +46,28 @@ const Books = () => {
           <div className="books__container">
             <div className="row">
               <div className="books__header">
-                <h2 className='section__title books__header--title'>All Books</h2>
-                <select id="filter" defaultValue="DEFAULT" onChange={(event) => filterBooks(event.target.value)}>
-                  <option value="DEFAULT" disabled>Sort</option>
-                  <option value="LOW_TO_HIGH">Price, Low to High</option>
-                  <option value="HIGH_TO_LOW">Price, High to Low</option>
+                <h2 className="section__title books__header--title">
+                  All Books
+                </h2>
+
+                <select
+                  id="filter"
+                  defaultValue="DEFAULT"
+                  onChange={(event) => filterBooks(event.target.value)}
+                >
+                  <option value="DEFAULT">Sort</option>
+                  <option value="LOW_TO_HIGH">
+                    Price, Low to High
+                  </option>
+                  <option value="HIGH_TO_LOW">
+                    Price, High to Low
+                  </option>
                   <option value="RATING">Rating</option>
                 </select>
               </div>
+
               <div className="books">
-                {books.map(book => (
+                {books.map((book) => (
                   <Book book={book} key={book.id} />
                 ))}
               </div>
@@ -44,7 +76,7 @@ const Books = () => {
         </section>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
